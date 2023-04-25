4778321e1e0fe3f95b52616ec854e3091c4dff72
festProbe <festprobe@gmail.com>
2023-04-25 19:42:43 +0300

    Add front and first components

---
+++ front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <header class="header"></header>
+    <main class="main">
+      <main-page
+      />
+    </main>
+    <footer class="footer"></footer>
+  </div>
+</template>
+
+<script>
+import MainPage from '@/pages/MainPage.vue'
+export default {
+  components: { MainPage }
+}
+</script>
+
+<style>
+
+</style>
